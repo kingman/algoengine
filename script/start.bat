b6e5241ee32f7d4f132ed7198c55e9577a500d35
unknown <chwa@Netlight-370.sto.netlight.se>
2013-02-18 22:15:46 +0100

    make the script start from root

--- script/start.bat
+++ script/start.bat
@@ -1,1 +1,1 @@
-java -jar ../target/standalone-netlight_algoengine-1.0-SNAPSHOT.jar ../src/main/resources/banzai.cfg
+java -jar target/standalone-netlight_algoengine-1.0-SNAPSHOT.jar src/main/resources/banzai.cfg
